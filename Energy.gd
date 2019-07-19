1384b74db0b8f3a853fa2d9ff9a62e96a2c8ced1
Oiboo <43292025+Oiboo@users.noreply.github.com>
2019-07-19 19:55:07 +0100

    update

--- Energy.gd
+++ Energy.gd
@@ -1,7 +1,10 @@
 extends "res://main.gd"
 
 
-func _on_Coin_body_entered(body):
+
+	
+
+func _on_Energy_body_entered(body):
 	if body.name == "Player":
 		get_node("/root/main").magic=10
 		queue_free()
